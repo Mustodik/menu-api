047d9983485df3116314d6d6f833ef49d5ccd8c6
Mustodik <dawbiemusdoiq30@gmail.com>
2022-04-26 12:38:09 +0000

    admin role

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,10 +1,10 @@
 const {Router} = require("express")
 const {createFood,getMenu} = require("../controllers/foodController")
-const {protect} =require("../middlewares/authMiddlewares")
+const {protect, admin} =require("../middlewares/authMiddlewares")
 
 const router =Router()
 
-router.route("/foods").post(protect, createFood).get(protect, getMenu)
+router.route("/foods").post(protect, admin, createFood).get(protect, getMenu)
 
 
 module.exports=router
